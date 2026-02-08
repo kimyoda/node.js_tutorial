6737f20d514097a64b89044fd5bb352f465a79ac
kimyoda <rladygks1210@naver.com>
2026-02-08 23:44:04 +0900

    [refactor] node_js_async/await 추가

--- section02/2.1_ES2015+/2_1_8-1.js
+++ section02/2.1_ES2015+/2_1_8-1.js
@@ -5,14 +5,18 @@
  * @param {user} Users  
  */
 
-// async function 교체-> 함수 선언부 앞에 async를 붙여 비동기 함수 임을 명시한다. 함수는 항상 Promise를 반환한다.
-// 내부에서 await를 사용할 수 있는 권한이 생긴다.
+// async function  추가, 기존 함수 선ㄷ언에 'async'를 붙여, 함수가 비동기 작업을 처리하는 것을 명시한다.
 async function findAndSaveUser(Users) {
-  // User.findOne({})이라는 Promise가 성공할때까지 함수 실행을 잠시 멈춘다.
+
+  // await 키워드 사용(.then 대체)
+  // 프로미스(Users.findOne)가 해결(resolve)될 때까지 기다린다.
+  // 결과값이 나오면 바로 user 변수에 대입한다.
   let user = await Users.findOne({});
+  // 콜백 함수가 필요 없이, 바로 변수를 수정하면 된다.
   user.name = 'zero';
-  // 저장된 최신 데이터를 다시 받아올 때 user 변수에 덮어버린다.
+  // 저장이 끝날때까지 기다렸다가, user 변수를 갱신한다.
   user = await user.save();
+  // 위 작업이 모두 끝나고 성별이 'm'인 유저를 조회한다.
   user = await Users.findOne({ gender: 'm' });
   // 생략
 }
